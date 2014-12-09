917046f416390f501aadd3e4a9143fdc834535c0
MITSUNARI Shigeo <herumi@nifty.com>
2014-12-09 14:31:54 +0900

    fix common.mk

--- common.mk
+++ common.mk
@@ -1,40 +1,45 @@
-GCC_VER=$(shell gcc -dumpversion)
-ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
-  CFLAGS_OPT +=-march=native
-endif
-ifeq ($(shell uname -s),Linux)
+GCC_VER=$(shell $(PRE)$(CC) -dumpversion)
+UNAME_S=$(shell uname -s)
+ifeq ($(UNAME_S),Linux)
   OS=Linux
 endif
-ifeq ($(CXX),clang++)
-  CFLAGS_OPT += -O3
-else
-  ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
-    CFLAGS_OPT +=-Ofast
-  else
-    CFLAGS_OPT += -O3
-  endif
-endif
-BIT=64
-ifeq ($(shell uname -o),Cygwin)
-  BIT=32
+ifneq ($(UNAME_S),Darwin)
+  LDFLAGS += -lrt
 endif
 CP = cp -f
 AR = ar r
 MKDIR=mkdir -p
 RM=rm -fr
-CFLAGS_OPT += -O3 -fomit-frame-pointer -DNDEBUG
-#CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wconversion -Wfloat-equal -Wpointer-arith #-Wswitch-enum -Wstrict-aliasing=2
-CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith #-Wswitch-enum -Wstrict-aliasing=2
-CFLAGS = -g -D_FILE_OFFSET_BITS=64 -msse4.2 -m$(BIT)
+CFLAGS_OPT+=-fomit-frame-pointer -DNDEBUG
+ifeq ($(CXX),clang++)
+  CFLAGS_OPT+=-O3
+else
+  ifeq ($(shell expr $(GCC_VER) \> 4.6.0),1)
+    CFLAGS_OPT+=-Ofast
+  else
+    CFLAGS_OPT+=-O3
+  endif
+endif
+CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith
+CFLAGS+= -g -D_FILE_OFFSET_BITS=64
 CFLAGS+=$(CFLAGS_WARN)
-LDFLAGS += -lpthread -lssl -m$(BIT) -lgmp -lgmpxx
-ifneq ($(shell uname -s),Darwin)
-  LDFLAGS += -lrt
+BIT?=64
+ifeq ($(BIT),0)
+	BIT_OPT=
+else
+	BIT_OPT=-m$(BIT)
+endif
+ifeq ($(MARCH),)
+ifeq ($(shell expr $(GCC_VER) \> 4.2.1),1)
+	CFLAGS+=-march=native
+endif
+else
+	CFLAGS+=$(MARCH)
 endif
 
 DEBUG=1
 ifeq ($(RELEASE),1)
-  DEBUG=0
+	DEBUG=0
 endif
 
 ifeq ($(DEBUG),0)
@@ -49,14 +54,18 @@ else
   OBJSUF=d
 endif
 
+####################################################
+
+LDFLAGS += -lpthread -lssl -m$(BIT) -lgmp -lgmpxx
+
+####################################################
+
 TOPDIR:=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))/
 EXTDIR:=$(TOPDIR)../cybozulib_ext/
-#CFLAGS+= -I$(TOPDIR)include -I$(EXTDIR)icu4c/icu/include -I$(EXTDIR)openssl/openssl/include
-#LDFLAGS+= -L$(TOPDIR)lib -lssl -lcrypto -ldl -licuuc -licudata -licui18n -lz -Wl,-rpath,'$$ORIGIN/../lib'
-CFLAGS+= -I$(TOPDIR)include  -I$(TOPDIR)../cybozulib/include/ -I$(TOPDIR)../xbyak/
-LDFLAGS+= -L$(TOPDIR)lib -Wl,-rpath,'$$ORIGIN/../lib'
+CFLAGS+= -I$(TOPDIR)include -I$(TOPDIR)../cybozulib/include/ -I$(TOPDIR)../xbyak/ $(BIT_OPT)
+LDFLAGS+= -L$(TOPDIR)lib $(BIT_OPT) -Wl,-rpath,'$$ORIGIN/../lib'
 
-MKDEP = sh -ec '$(CC) -MM $(CFLAGS) $< | sed "s@\($*\)\.o[ :]*@$(OBJDIR)/\1.o $@ : @g" > $@; [ -s $@ ] || rm -f $@'
+MKDEP = sh -ec '$(PRE)$(CC) -MM $(CFLAGS) $< | sed "s@\($*\)\.o[ :]*@$(OBJDIR)/\1.o $@ : @g" > $@; [ -s $@ ] || rm -f $@; touch $@'
 
 CLEAN=$(RM) $(TARGET) $(OBJDIR)
 
@@ -72,13 +81,13 @@ endef
 .SUFFIXES: .cpp .d .exe
 
 $(OBJDIR)/%.o: %.cpp
-	$(CXX) -c $< -o $@ $(CFLAGS)
+	$(PRE)$(CXX) -c $< -o $@ $(CFLAGS)
 
 $(OBJDIR)/%.d: %.cpp $(OBJDIR)
 	@$(MKDEP)
 
 $(TOPDIR)bin/%$(OBJSUF).exe: $(OBJDIR)/%.o $(LIBS)
-	$(CXX) $< -o $@ $(LIBS) $(LDFLAGS)
+	$(PRE)$(CXX) $< -o $@ $(LIBS) $(LDFLAGS)
 
 OBJS=$(addprefix $(OBJDIR)/,$(SRC:.cpp=.o))
 
