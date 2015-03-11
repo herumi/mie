0ba1609d7b27bd6b77121bd76bf4fb58be4acd5b
MITSUNARI Shigeo <herumi@nifty.com>
2015-03-12 06:21:29 +0900

    add mulNxN

--- src/gen.py
+++ src/gen.py
@@ -17,6 +17,37 @@ def gen_mulNx1(fo, unitN, n):
 	print>>fo, "  ret i%d %%t%d" % (outN, n - 2)
 	print>>fo, "}"
 
+def gen_mulNxN(fo, unitN, n):
+	inN = unitN * n
+	bitNpU = inN + unitN
+	outN = inN * 2
+	print>>fo, "define void @mul%dx%d(i%d* %%pz, i%d* %%px, i%d* %%py) {" % (inN, inN, unitN, inN, inN)
+	print>>fo, "  %%x = load i%d* %%px" % inN
+	print>>fo, "  %%y = load i%d* %%py" % inN
+	for i in xrange(0, n):
+		print>>fo, "  %%y%d = call i%d @extract%d(i%d %%y, i%d %d)" % (i, unitN, inN, inN, inN, i * unitN)
+
+	print>>fo, "  %%sum0 = call i%d @mul%dx%d(i%d %%x, i%d %%y0)" % (bitNpU, inN, unitN, inN, unitN)
+	print>>fo, "  %%t0 = trunc i%d %%sum0 to i%d" % (bitNpU, unitN)
+	print>>fo, "  store i%d %%t0, i%d* %%pz" % (unitN, unitN)
+
+	for i in xrange(1, n):
+		print>>fo
+		print>>fo, "  %%s%d = lshr i%d %%sum%d, %d" % (i - 1, bitNpU, i - 1, unitN)
+		print>>fo, "  %%xy%d = call i%d @mul%dx%d(i%d %%x, i%d %%y%d)" % (i, bitNpU, inN, unitN, inN, unitN, i)
+		print>>fo, "  %%sum%d = add i%d %%s%d, %%xy%d" % (i, bitNpU, i - 1, i)
+		print>>fo, "  %%z%d = getelementptr i%d* %%pz, i32 %d" % (i, unitN, i)
+		if i == n - 1:
+			break
+		print>>fo, "  %%ts%d = trunc i%d %%sum%d to i%d" % (i, bitNpU, i, unitN)
+		print>>fo, "  store i%d %%ts%d, i%d* %%z%d" % (unitN, i, unitN, i)
+
+	print>>fo, "  %%p = bitcast i%d* %%z%d to i%d*" % (unitN, n - 1, bitNpU)
+	print>>fo, "  store i%d %%sum%d, i%d* %%p" % (bitNpU, n - 1, bitNpU)
+	print>>fo, "  ret void"
+	print>>fo, "}"
+
+
 def gen_sub(fo, s, unitN, bitN):
 	s = s.replace('@bitNpUm1', str(bitN + unitN - 1))
 	s = s.replace('@bitNpU', str(bitN + unitN))
@@ -55,6 +86,7 @@ def main():
 		exit(1)
 	for i in xrange(2, 4):
 		gen_mulNx1(fo, unitN, i)
+	gen_mulNxN(fo, unitN, 3)
 	fo.close()
 
 if __name__ == "__main__":
