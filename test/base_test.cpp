ac0f895019fbc6818405c1c911410e993eac83ee
MITSUNARI Shigeo <herumi@nifty.com>
2015-03-22 06:54:39 +0900

    add Montgomery for base_test

--- test/base_test.cpp
+++ test/base_test.cpp
@@ -23,6 +23,61 @@ size_t getUnitN(size_t bitLen)
 	return (bitLen + sizeof(Unit) * 8 - 1) / (sizeof(Unit) * 8);
 }
 
+struct Montgomery {
+	typedef mie::Gmp::BlockType BlockType;
+	mpz_class p_;
+	mpz_class R_; // (1 << (pn_ * 64)) % p
+	mpz_class RR_; // (R * R) % p
+	BlockType pp_; // p * pp = -1 mod M = 1 << 64
+	size_t pn_;
+	Montgomery() {}
+	explicit Montgomery(const mpz_class& p)
+	{
+		p_ = p;
+		pp_ = mie::montgomery::getCoff(mie::Gmp::getBlock(p, 0));
+		pn_ = mie::Gmp::getBlockSize(p);
+		R_ = 1;
+		R_ = (R_ << (pn_ * 64)) % p_;
+		RR_ = (R_ * R_) % p_;
+	}
+
+	void toMont(mpz_class& x) const { mul(x, x, RR_); }
+	void fromMont(mpz_class& x) const { mul(x, x, 1); }
+
+	void mul(mpz_class& z, const mpz_class& x, const mpz_class& y) const
+	{
+#if 0
+		const size_t ySize = mie::Gmp::getBlockSize(y);
+		mpz_class c = x * mie::Gmp::getBlock(y, 0);
+		BlockType q = mie::Gmp::getBlock(c, 0) * pp_;
+		c += p_ * q;
+		c >>= sizeof(BlockType) * 8;
+		for (size_t i = 1; i < pn_; i++) {
+			if (i < ySize) {
+				c += x * mie::Gmp::getBlock(y, i);
+			}
+			BlockType q = mie::Gmp::getBlock(c, 0) * pp_;
+			c += p_ * q;
+			c >>= sizeof(BlockType) * 8;
+		}
+		if (c >= p_) {
+			c -= p_;
+		}
+		z = c;
+#else
+		z = x * y;
+		for (size_t i = 0; i < pn_; i++) {
+			BlockType q = mie::Gmp::getBlock(z, 0) * pp_;
+			z += p_ * (mp_limb_t)q;
+			z >>= sizeof(BlockType) * 8;
+		}
+		if (z >= p_) {
+			z -= p_;
+		}
+#endif
+	}
+};
+
 void setMpz(mpz_class& mx, const Unit *x, size_t n)
 {
 	mie::Gmp::setRaw(mx, x, n);
