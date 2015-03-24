6ec56c8a8c21045f4d11e9d7bf0b34d5d2b1b927
MITSUNARI Shigeo <herumi@nifty.com>
2015-03-25 05:12:47 +0900

    rename unitN to unitL

--- src/gen.py
+++ src/gen.py
@@ -1,62 +1,62 @@
 import sys, re
 
-def gen_mulNx1(fo, unitN, n):
-	inN = unitN * n
-	outN = unitN * (n + 1)
+def gen_mulNx1(fo, unitL, n):
+	inL = unitL * n
+	outL = unitL * (n + 1)
 	if n > 4:
 		attr = "noinline"
 	else:
 		attr = ""
-	print>>fo, "define private i%d @mul%dx%d(i%d %%x, i%d %%y) %s {" % (outN, inN, unitN, inN, unitN, attr)
+	print>>fo, "define private i%d @mul%dx%d(i%d %%x, i%d %%y) %s {" % (outL, inL, unitL, inL, unitL, attr)
 	for i in xrange(0, n):
-		print>>fo, "  %%x%d = call i%d @extract%d(i%d %%x, i%d %d)" % (i, unitN, inN, inN, inN, i * unitN)
-		print>>fo, "  %%x%dy = call i%d @mul%dx%d(i%d %%x%d, i%d %%y)" % (i, unitN * 2, unitN, unitN, unitN, i, unitN)
-		print>>fo, "  %%x%dy0 = zext i%d %%x%dy to i%d" % (i, unitN * 2, i, outN)
+		print>>fo, "  %%x%d = call i%d @extract%d(i%d %%x, i%d %d)" % (i, unitL, inL, inL, inL, i * unitL)
+		print>>fo, "  %%x%dy = call i%d @mul%dx%d(i%d %%x%d, i%d %%y)" % (i, unitL * 2, unitL, unitL, unitL, i, unitL)
+		print>>fo, "  %%x%dy0 = zext i%d %%x%dy to i%d" % (i, unitL * 2, i, outL)
 	for i in xrange(1, n):
-		print>>fo, "  %%x%dy1 = shl i%d %%x%dy0, %d" % (i, outN, i, i * unitN)
+		print>>fo, "  %%x%dy1 = shl i%d %%x%dy0, %d" % (i, outL, i, i * unitL)
 
-	print>>fo, "  %%t0 = add i%d %%x0y0, %%x1y1" % outN
+	print>>fo, "  %%t0 = add i%d %%x0y0, %%x1y1" % outL
 	for i in xrange(1, n - 1):
-		print>>fo, "  %%t%d = add i%d %%t%d, %%x%dy1" % (i, outN, i - 1, i + 1)
-	print>>fo, "  ret i%d %%t%d" % (outN, n - 2)
+		print>>fo, "  %%t%d = add i%d %%t%d, %%x%dy1" % (i, outL, i - 1, i + 1)
+	print>>fo, "  ret i%d %%t%d" % (outL, n - 2)
 	print>>fo, "}"
 
-def gen_mulNxN(fo, unitN, n):
-	inN = unitN * n
-	bitNpU = inN + unitN
-	outN = inN * 2
-	print>>fo, "define void @mie_fp_mul%dpre(i%d* %%pz, i%d* %%px, i%d* %%py) {" % (inN, unitN, inN, inN)
-	print>>fo, "  %%x = load i%d* %%px" % inN
-	print>>fo, "  %%y = load i%d* %%py" % inN
+def gen_mulNxN(fo, unitL, n):
+	inL = unitL * n
+	bitLpU = inL + unitL
+	outL = inL * 2
+	print>>fo, "define void @mie_fp_mul%dpre(i%d* %%pz, i%d* %%px, i%d* %%py) {" % (inL, unitL, inL, inL)
+	print>>fo, "  %%x = load i%d* %%px" % inL
+	print>>fo, "  %%y = load i%d* %%py" % inL
 	for i in xrange(0, n):
-		print>>fo, "  %%y%d = call i%d @extract%d(i%d %%y, i%d %d)" % (i, unitN, inN, inN, inN, i * unitN)
+		print>>fo, "  %%y%d = call i%d @extract%d(i%d %%y, i%d %d)" % (i, unitL, inL, inL, inL, i * unitL)
 
-	print>>fo, "  %%sum0 = call i%d @mul%dx%d(i%d %%x, i%d %%y0)" % (bitNpU, inN, unitN, inN, unitN)
-	print>>fo, "  %%t0 = trunc i%d %%sum0 to i%d" % (bitNpU, unitN)
-	print>>fo, "  store i%d %%t0, i%d* %%pz" % (unitN, unitN)
+	print>>fo, "  %%sum0 = call i%d @mul%dx%d(i%d %%x, i%d %%y0)" % (bitLpU, inL, unitL, inL, unitL)
+	print>>fo, "  %%t0 = trunc i%d %%sum0 to i%d" % (bitLpU, unitL)
+	print>>fo, "  store i%d %%t0, i%d* %%pz" % (unitL, unitL)
 
 	for i in xrange(1, n):
 		print>>fo
-		print>>fo, "  %%s%d = lshr i%d %%sum%d, %d" % (i - 1, bitNpU, i - 1, unitN)
-		print>>fo, "  %%xy%d = call i%d @mul%dx%d(i%d %%x, i%d %%y%d)" % (i, bitNpU, inN, unitN, inN, unitN, i)
-		print>>fo, "  %%sum%d = add i%d %%s%d, %%xy%d" % (i, bitNpU, i - 1, i)
-		print>>fo, "  %%z%d = getelementptr i%d* %%pz, i32 %d" % (i, unitN, i)
+		print>>fo, "  %%s%d = lshr i%d %%sum%d, %d" % (i - 1, bitLpU, i - 1, unitL)
+		print>>fo, "  %%xy%d = call i%d @mul%dx%d(i%d %%x, i%d %%y%d)" % (i, bitLpU, inL, unitL, inL, unitL, i)
+		print>>fo, "  %%sum%d = add i%d %%s%d, %%xy%d" % (i, bitLpU, i - 1, i)
+		print>>fo, "  %%z%d = getelementptr i%d* %%pz, i32 %d" % (i, unitL, i)
 		if i == n - 1:
 			break
-		print>>fo, "  %%ts%d = trunc i%d %%sum%d to i%d" % (i, bitNpU, i, unitN)
-		print>>fo, "  store i%d %%ts%d, i%d* %%z%d" % (unitN, i, unitN, i)
+		print>>fo, "  %%ts%d = trunc i%d %%sum%d to i%d" % (i, bitLpU, i, unitL)
+		print>>fo, "  store i%d %%ts%d, i%d* %%z%d" % (unitL, i, unitL, i)
 
-	print>>fo, "  %%p = bitcast i%d* %%z%d to i%d*" % (unitN, n - 1, bitNpU)
-	print>>fo, "  store i%d %%sum%d, i%d* %%p" % (bitNpU, n - 1, bitNpU)
+	print>>fo, "  %%p = bitcast i%d* %%z%d to i%d*" % (unitL, n - 1, bitLpU)
+	print>>fo, "  store i%d %%sum%d, i%d* %%p" % (bitLpU, n - 1, bitLpU)
 	print>>fo, "  ret void"
 	print>>fo, "}"
 
-def gen_mul(fo, unitN):
-	bitN = 576
-	n = (bitN + unitN - 1) / unitN
+def gen_mul(fo, unitL):
+	bitL = 576
+	n = (bitL + unitL - 1) / unitL
 	for i in xrange(2, n + 1):
-		gen_mulNx1(fo, unitN, i)
-		gen_mulNxN(fo, unitN, i)
+		gen_mulNx1(fo, unitL, i)
+		gen_mulNxN(fo, unitL, i)
 
 RE_VAL = re.compile(r'\$\(([^)]+)\)')
 
@@ -65,16 +65,16 @@ def evalStr(s, envG, envL={}):
 		return str(eval(x.group(1), envG, envL))
 	return RE_VAL.sub(eval2str, s)
 
-def expandFor(s, unitN, bitN):
+def expandFor(s, unitL, bitL):
 	out = []
 	inFor = False
 	b = 0
 	e = 0
-	N = bitN / unitN
+	N = bitL / unitL
 	envG = {
 		'N' : N,
-		'unitN' : unitN,
-		'bitN' : bitN
+		'unitL' : unitL,
+		'bitL' : bitL
 	}
 	RE_FOR = re.compile(r'@for\s+([^ ]+)\s*,\s*([^ ]+)')
 	for line in s.split('\n'):
@@ -106,44 +106,44 @@ def expandFor(s, unitN, bitN):
 				out.append(evalStr(line, envG))
 	return '\n'.join(out)
 
-def gen_sub(fo, s, unitN, bitN):
-	bitNpU = bitN + unitN
-	s = s.replace('@bitNpUm1', str(bitNpU - 1))
-	s = s.replace('@bitNpU', str(bitNpU))
-	s = s.replace('@bitNm1', str(bitN - 1))
-	s = s.replace('@bitN', str(bitN))
-	s = s.replace('@unitN2', str(unitN * 2))
-	s = s.replace('@unitN', str(unitN))
-	s = expandFor(s, unitN, bitN)
+def gen_sub(fo, s, unitL, bitL):
+	bitLpU = bitL + unitL
+	s = s.replace('@bitLpUm1', str(bitLpU - 1))
+	s = s.replace('@bitLpU', str(bitLpU))
+	s = s.replace('@bitLm1', str(bitL - 1))
+	s = s.replace('@bitL', str(bitL))
+	s = s.replace('@unitL2', str(unitL * 2))
+	s = s.replace('@unitL', str(unitL))
+	s = expandFor(s, unitL, bitL)
 	fo.write(s)
 
-def gen(fo, inName, unitN, bitNL):
-	fi = open(inName, 'r')
+def gen(fo, inLame, unitL, bitLL):
+	fi = open(inLame, 'r')
 	s = fi.read()
 	fi.close()
-	for bitN in bitNL:
-		gen_sub(fo, s, unitN, bitN)
+	for bitL in bitLL:
+		gen_sub(fo, s, unitL, bitL)
 
 def main():
 	argv = sys.argv
 	args = len(argv)
-	unitN = 64
+	unitL = 64
 	if args == 2:
-		unitN = int(argv[1])
-	if unitN not in [32, 64]:
-		print "bad unitN", unitN
+		unitL = int(argv[1])
+	if unitL not in [32, 64]:
+		print "bad unitL", unitL
 		exit(1)
 
-	outName = 'base%d.ll' % unitN
-	fo = open(outName, 'w')
-	gen(fo, 'once.txt', unitN, [unitN * 2])
+	outLame = 'base%d.ll' % unitL
+	fo = open(outLame, 'w')
+	gen(fo, 'once.txt', unitL, [unitL * 2])
 
-	bitNL = range(unitN, 576 + 1, unitN)
-	gen(fo, 'all.txt', unitN, bitNL)
-	gen(fo, 'short.txt', unitN, bitNL)
-	gen(fo, 'long.txt', unitN, bitNL)
-#	gen(fo, 'mul.txt', unitN, bitNL)
-	gen_mul(fo, unitN)
+	bitLL = range(unitL, 576 + 1, unitL)
+	gen(fo, 'all.txt', unitL, bitLL)
+	gen(fo, 'short.txt', unitL, bitLL)
+	gen(fo, 'long.txt', unitL, bitLL)
+#	gen(fo, 'mul.txt', unitL, bitLL)
+	gen_mul(fo, unitL)
 	fo.close()
 
 if __name__ == "__main__":
